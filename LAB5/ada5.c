107af54422e300b150e7282d592518bfc7fb8885
anisha3010 <63083316+anisha3010@users.noreply.github.com>
2021-04-28 22:30:59 +0530

    Update ada5.c

--- LAB5/ada5.c
+++ LAB5/ada5.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-
+#include <time.h>
 int main()
 {
+    clock_t start,end;
+    double timetaken;
+	int rand(void);
     int i,j,n,key;
     printf("Enter the number of elements\n");
     scanf("%d",&n);
     int arr[n];
-    printf("Enter the array\n");
+    printf("Printing random valued array\n");
     for(i=0;i<n;i++)
     {
-        scanf("%d",&arr[i]);
+        arr[i]=rand() % 1000 + 1;
+        printf("%d\t",arr[i]);
     }
     printf("Sorting the array using insertion sort\n");
+    start=clock();
     for (i = 1; i < n; i++) 
     {
         key = arr[i];
@@ -23,11 +28,13 @@ int main()
         }
         arr[j + 1] = key;
     }
+     end=clock();
     printf("Array has been sorted as...\n");
     for (i = 0; i < n; i++) 
     { 
-        printf("%d\n",arr[i]);
+        printf("%d\t",arr[i]);
     }
-
+	timetaken=((double)(end-start))/CLOCKS_PER_SEC;
+    printf("\ntime taken = %f",timetaken);
     return 0;
 }
